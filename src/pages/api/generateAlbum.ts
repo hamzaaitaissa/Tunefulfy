e46c976a6e55a7b5db9535191f07071fcf5bb00e
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2023-12-24 01:32:08 +0100

    api

--- src/pages/api/generateAlbum.ts
+++ src/pages/api/generateAlbum.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const API = process.env.API_KEY
   try {
-    const response = await axios.get('https://1001albumsgenerator.com/api/v1/projects/658716ae1dcb2b08e6e37c32');
+    const response = await axios.get(API!);
     res.status(200).json(response.data.currentAlbum);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
