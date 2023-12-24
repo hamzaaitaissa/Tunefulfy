9c1e0d63bc474a23f8c45196dc457616cf89d739
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2023-12-24 01:42:00 +0100

    env

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["i.scdn.co"], // Add 'i.scdn.co' to the list of allowed domains
   },
+  env: {  API_KEY: process.env.API_KEY }
 };
 
 module.exports = nextConfig;
