a6f3e751a8ef2fa0ec5f291f33454525bbb176e0
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2023-12-24 02:21:30 +0100

    api-key

--- src/pages/api/generateAlbum.ts
+++ src/pages/api/generateAlbum.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const API = process.env.NEXT_PUBLIC_API_KEY
+
   try {
     const response = await axios.get(process.env.NEXT_PUBLIC_API_KEY!);
     res.status(200).json(response.data.currentAlbum);
