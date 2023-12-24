858ae92f4eb9492ef3f65a85acfb2517f57f91c8
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2023-12-24 01:58:02 +0100

    API

--- src/pages/api/generateAlbum.ts
+++ src/pages/api/generateAlbum.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const API = process.env.API_KEY
   try {
-    const response = await axios.get(API!);
+    const response = await axios.get(process.env.NEXT_PUBLIC_API_KEY!);
     res.status(200).json(response.data.currentAlbum);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
