fc6d45b451cc3605230756fbd6418a7c2f17824f
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-03-30 01:58:26 +0000

    Update generateAlbum.ts to fetch current album using API key

--- src/pages/api/generateAlbum.ts
+++ src/pages/api/generateAlbum.ts
@@ -6,7 +6,11 @@ import axios from 'axios';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   try {
-    const response = await axios.get('https://1001albumsgenerator.com/api/v1/projects/658716ae1dcb2b08e6e37c32');
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    if (!apiKey) {
+      throw new Error('API key is not defined in environment variables');
+    }
+    const response = await axios.get(`https://1001albumsgenerator.com/api/v1/projects/${apiKey}`);
     res.status(200).json(response.data.currentAlbum);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
